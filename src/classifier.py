fd2f31935f18b86a0093ae50e9438589a4d23beb
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-03-19 10:02:45 +0100

    Helper script

--- src/classifier.py
+++ src/classifier.py
@@ -13,17 +13,21 @@ import matplotlib.pyplot as plt
 
 def run():
     classifier = Classifier(load_model(in_out.get_parameters()["paths"]["model"]))
-    nb_features = 756
-    nb_samples = 1000000
-    random_samples = np.random.rand((nb_samples * nb_features)).reshape(
-        nb_samples, nb_features
-    )
+    # nb_features = 756
+    # nb_samples = 1000000
+    # random_samples = np.random.rand((nb_samples * nb_features)).reshape(
+    #     nb_samples, nb_features
+    # )
+    X_initial_states = np.load(CONFIG["paths"]["x_candidates"])
+    print(X_initial_states.shape)
 
-    y_pred = classifier.predict_proba(random_samples)
+    y_pred = classifier.predict_proba(X_initial_states)
+    print(y_pred)
     # print(y_pred[:10])
-    # sns.distplot(y_pred, hist=True, kde=True, kde_kws={"linewidth": 3}, label="Class")
-    # plt.show()
+    sns.distplot(y_pred, hist=True, kde=True, kde_kws={"linewidth": 3}, label="Class")
+    plt.show()
     print((y_pred == 0).sum())
+
 
 
 if __name__ == "__main__":
