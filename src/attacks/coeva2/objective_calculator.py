7e617039aa22672a6919c4db77d0b7cad8140f45
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-04-14 15:01:00 +0200

    Explaining adv

--- src/attacks/coeva2/objective_calculator.py
+++ src/attacks/coeva2/objective_calculator.py
@@ -1,5 +1,5 @@
 import pickle
-from typing import List
+from typing import List, Dict
 
 from attacks.coeva2.classifier import Classifier
 from attacks.coeva2.constraints import Constraints
@@ -9,8 +9,10 @@ from attacks.coeva2.feature_encoder import get_encoder_from_constraints
 from attacks.coeva2.result_process import EfficientResult
 import sys
 import numpy
+
 numpy.set_printoptions(threshold=sys.maxsize)
 from utils import in_out
+
 config = in_out.get_parameters()
 
 
@@ -20,17 +22,17 @@ class ObjectiveCalculator:
         classifier: Classifier,
         constraints: Constraints,
         threshold: float,
-        #high_amount: int,
-        #amount_index: int,
+        # high_amount: int,
+        # amount_index: int,
     ):
         self._classifier = classifier
         self._constraints = constraints
         self._encoder = get_encoder_from_constraints(constraints)
         self._threshold = threshold
-        #self._high_amount = high_amount
-        #self._amount_index = amount_index
+        # self._high_amount = high_amount
+        # self._amount_index = amount_index
         if config["paths"]["ml_scaler"] is not None:
-            with open(config["paths"]["ml_scaler"], 'rb') as f:
+            with open(config["paths"]["ml_scaler"], "rb") as f:
                 self._ml_scaler = pickle.load(f)
         else:
             self._ml_scaler = None
@@ -40,8 +42,7 @@ class ObjectiveCalculator:
         x_ml = self._encoder.genetic_to_ml(x, result.initial_state)
 
         respectsConstraints = (
-            (self._constraints.evaluate(x_ml)).sum(axis=1)
-            <= 0
+            (self._constraints.evaluate(x_ml)).sum(axis=1) <= 0
         ).astype(np.int64)
         # print(self._constraints.evaluate(x_ml))
 
@@ -55,14 +56,14 @@ class ObjectiveCalculator:
         # f1 = np.array(f1).flatten()
         isMisclassified = np.array(f1 < self._threshold).astype(np.int64)
 
-        #isHighAmount = (x_ml[:, self._amount_index] >= self._high_amount).astype(
-            #np.int64
-        #)
+        # isHighAmount = (x_ml[:, self._amount_index] >= self._high_amount).astype(
+        # np.int64
+        # )
 
         o1 = respectsConstraints
         o2 = isMisclassified
         o3 = o1 * o2
-        #o4 = o3 * isHighAmount
+        # o4 = o3 * isHighAmount
         return np.array([respectsConstraints, isMisclassified, o3])
 
     def _objective_per_initial_sample(self, result: EfficientResult):
@@ -90,3 +91,24 @@ class ObjectiveCalculator:
             training.append(x_ml[adv_filter])
 
         return np.concatenate(training)
+
+    def get_generated(
+        self, results: List[EfficientResult]
+    ) -> List[Dict[str, np.ndarray]]:
+        generated = []
+
+        for result in results:
+            adv_filter = self._objective_per_individual(result)[2].astype(np.bool)
+            x = np.array(list(map(lambda e: e.X, result.pop))).astype(np.float64)
+            x_ml = self._encoder.genetic_to_ml(x, result.initial_state)
+            x_adv_success = x_ml[adv_filter]
+            x_adv_failure = x_ml[~adv_filter]
+            generated.append(
+                {
+                    "x_initial_state": result.initial_state,
+                    "x_adv_success": x_adv_success,
+                    "x_adv_fail": x_adv_failure,
+                }
+            )
+
+        return generated
