34672b0c6b930c910c1f828c02f48a5e1d54e20f
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-06-10 14:55:45 +0200

    Remove duplicated files now in united

--- src/examples/lcld/lcld_problem.py
+++ src/examples/lcld/lcld_problem.py
@@ -2,14 +2,17 @@ import copy
 
 from src.attacks.moeva2.default_problem import DefaultProblem
 
-
 AVOID_ZERO = 0.00000001
 NB_OBJECTIVES = 3
 
 
+##############################
+#                          ###
+# THIS IS NOT RELEVANT YET ###
+#                          ###
+##############################
 class LcldProblem(DefaultProblem):
     def _evaluate_additional_objectives(self, x, x_f, x_f_mm, x_ml):
-
         # Maximize the amount
         amount_feature_index = 0
         amount = copy.deepcopy(x_ml[:, amount_feature_index])
