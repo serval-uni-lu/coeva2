22a50ba2282cfa1b8ca141d63ca82a9d058b2e82
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-06-10 14:12:24 +0200

    Remove printing

--- src/attacks/moeva2/objective_calculator.py
+++ src/attacks/moeva2/objective_calculator.py
@@ -61,6 +61,7 @@ class ObjectiveCalculator:
         x_i_scaled = self._min_max_scaler.transform(x_initial.reshape(1, -1))
         x_scaled = self._min_max_scaler.transform(x_f)
         tol = 0.0001
+        # x_scaled = np.clip(x_scaled, 0, 1)
         assert np.all(x_i_scaled >= 0 - tol)
         assert np.all(x_i_scaled <= 1 + tol)
         assert np.all(x_scaled >= 0 - tol)
@@ -74,7 +75,7 @@ class ObjectiveCalculator:
         # print(np.min(l2[(constraints_respected * misclassified)]))
 
         l2_in_ball = l2 <= self._thresholds["f2"]
-        # print(l2)
+        print(l2)
         # print(np.min(l2))
         # Additional
         # to implement
