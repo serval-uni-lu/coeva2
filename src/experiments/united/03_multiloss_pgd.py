2969c144bc2a1e9f0ff3f38b413915a1205f8b4c
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-07-29 14:25:14 +0200

    Code formating

--- src/experiments/united/03_multiloss_pgd.py
+++ src/experiments/united/03_multiloss_pgd.py
@@ -39,10 +39,10 @@ from src.attacks.moeva2.objective_calculator import ObjectiveCalculator
 
 def run(params={}):
     experiment = None
-    enable_comet = config.get("comet",True)
+    enable_comet = config.get("comet", True)
     if enable_comet:
-        p = {"approach":"pgd","nb_iter":1000 }
-        params = {**p, **config , **params}
+        p = {"approach": "pgd", "nb_iter": 1000}
+        params = {**p, **config, **params}
         experiment = init_comet(params)
 
     Path(config["paths"]["attack_results"]).parent.mkdir(parents=True, exist_ok=True)
@@ -85,19 +85,19 @@ def run(params={}):
         input_shape=initial_shape,
         loss_object=tf.keras.losses.binary_crossentropy,
         nb_classes=2,
-        constraints = constraints,
-        scaler = scaler,
+        constraints=constraints,
+        scaler=scaler,
         experiment=experiment,
-        parameters = params
+        parameters=params,
     )
     pgd = PGD(
         kc_classifier,
-        eps=config["thresholds"]["f2"]-0.000001,
+        eps=config["thresholds"]["f2"] - 0.000001,
         eps_step=config["thresholds"]["f2"] / 300,
         norm=params.get("norm"),
         verbose=True,
         max_iter=params.get("nb_iter"),
-        num_random_init=params.get("nb_random",0)
+        num_random_init=params.get("nb_random", 0),
     )
     X_initial_states = scaler.transform(x_initial)
     attacks = pgd.generate(
@@ -112,7 +112,6 @@ def run(params={}):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
-
 
     if experiment:
         results = np.load(config["paths"]["attack_results"])
@@ -145,18 +144,30 @@ def run(params={}):
 
 
 if __name__ == "__main__":
-    run({"constraints_optim":"constraints+flip+alternate", "nb_iter":1000, "nb_random":1,
-         "n_initial_state":100, "alternate_frequency":5})
+    run(
+        {
+            "constraints_optim": "constraints+flip+alternate",
+            "nb_iter": 1000,
+            "nb_random": 1,
+            "n_initial_state": 100,
+            "alternate_frequency": 5,
+        }
+    )
 
-    #To allow the metrics to be uploaded
+    # To allow the metrics to be uploaded
     time.sleep(30)
     exit()
-    strategies= ["single_constraints+flip","constraints+flip","single_constraints", "constraints+flip+alternate",
-                 "constraints+flip+constraints"]
+    strategies = [
+        "single_constraints+flip",
+        "constraints+flip",
+        "single_constraints",
+        "constraints+flip+alternate",
+        "constraints+flip+constraints",
+    ]
     for str in strategies:
         nb_ctr = 10 if "single" in str else 1
         for ctr in range(nb_ctr):
-            print("ctr",ctr)
+            print("ctr", ctr)
             # constraints_optim: how to optimise constraints:
             #
-            run({"ctr_id":ctr,"constraints_optim":str, "nb_iter":100})
+            run({"ctr_id": ctr, "constraints_optim": str, "nb_iter": 100})
