c6f458ad58d46c340768f6c17edc081ab339d7ba
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-08-05 11:22:38 +0200

    Moeva using new config file

--- src/experiments/united/04_moeva.py
+++ src/experiments/united/04_moeva.py
@@ -5,20 +5,23 @@ from pathlib import Path
 import joblib
 import numpy as np
 
+from src.attacks.moeva2.feature_encoder import get_encoder_from_constraints
 from src.attacks.moeva2.moeva2 import Moeva2
+from src.attacks.moeva2.utils import results_to_numpy_results, results_to_history
+from src.config_parser.config_parser import get_config, get_config_hash, save_config
 from src.experiments.united.utils import get_constraints_from_str
-from src.utils import Pickler, in_out, filter_initial_states, timing
+from src.utils import Pickler, filter_initial_states, timing
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.simplefilter(action="ignore", category=RuntimeWarning)
 
-config = in_out.get_parameters()
+# config = in_out.get_parameters()
 
 
 @timing
 def run():
 
-    Path(config["paths"]["attack_results"]).parent.mkdir(parents=True, exist_ok=True)
+    Path(config['dirs']['results']).parent.mkdir(parents=True, exist_ok=True)
 
     save_history = True
     if "save_history" in config:
@@ -43,39 +46,53 @@ def run():
     constraints.check_constraints_error(X_initial_states)
 
     # ----- Copy the initial states n_repetition times
-    X_initial_states = np.repeat(X_initial_states, config["n_repetition"], axis=0)
+    # X_initial_states = np.repeat(X_initial_states, config["n_repetition"], axis=0)
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
 
-    # scaler = joblib.load(config["paths"]["min_max_scaler"])
+    n_gen = int(np.floor(config["budget"] / config["n_offsprings"]))
 
     moeva = Moeva2(
         config["paths"]["model"],
         constraints,
         problem_class=None,
-        l2_ball_size=config["l2_ball_size"],
+        l2_ball_size=0.,
         norm=config["norm"],
-        n_gen=config["n_gen"],
+        n_gen=n_gen,
         n_pop=config["n_pop"],
         n_offsprings=config["n_offsprings"],
         scale_objectives=True,
         save_history=save_history,
         seed=config["seed"],
-        n_jobs=config["n_jobs"],
+        n_jobs=config["system"]["n_jobs"],
         ml_scaler=scaler,
         verbose=1,
     )
-
     attacks = moeva.generate(X_initial_states, 1)
 
-    Pickler.save_to_file(attacks, config["paths"]["attack_results"])
+    # Save
+    config_hash = get_config_hash()
+    out_dir = config['dirs']['results']
+    # Legacy
+    Pickler.save_to_file(attacks, f"{out_dir}/results_{config_hash}.npy")
 
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
+    # Attacks crafted
+    x_attacks = results_to_numpy_results(
+        attacks, get_encoder_from_constraints(constraints)
+    )
+    np.save(f"{out_dir}/x_attacks_{config_hash}.npy", x_attacks)
+
+    # History
+    if save_history:
+        x_histories = results_to_history(attacks)
+        np.save(f"{out_dir}/x_histories_{config_hash}.npy", x_histories)
+
+    # Config
+    save_config(f"{out_dir}/config_")
 
 
 if __name__ == "__main__":
+    config = get_config()
     run()
