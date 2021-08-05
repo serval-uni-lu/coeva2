538776757b24f577dd2c1747491e1d5a643f6535
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-08-05 16:18:22 +0200

    Add condition on attack run meta runner

--- src/run_all_copy.py
+++ src/run_all_copy.py
@@ -1,5 +1,8 @@
+import json
 import os
 import logging
+import subprocess
+
 from src.config_parser.config_parser import get_config, merge_parameters
 
 TABULATOR = ">>>"
@@ -10,43 +13,50 @@ def launch_script(script):
     global launch_counter
     launch_counter += 1
     logger.info(script)
+    subprocess.run(script)
 
 
 def run():
     config_dir = config["config_dir"]
-    for seed in ["seeds"]:
+    for seed in config["seeds"]:
         logger.info(f"{TABULATOR*1} Running seed {seed} ...")
         for project in config["projects"]:
             logger.info(f"{TABULATOR*2} Running project {project} ...")
             for budget in config["budgets"]:
                 logger.info(f"{TABULATOR * 3} Running budget {budget} ...")
 
-                # Run MoEvA
-                launch_script(
-                    f"python -m src.experiments.united.04_moeva "
-                    f"-c {config_dir}/moeva.yaml "
-                    f"-c {config_dir}/{project}.yaml "
-                    f"-p seed={seed} "
-                    f"-p budget={budget} "
-                )
+                if "moeva" in config["attacks"]:
+                    logger.info(f"{TABULATOR * 4} Running MoEvA ...")
+                    eps_list = {"eps_list": config['eps_list']}
+                    eps_list_str = json.dumps(eps_list, separators=(',', ':')).replace("\"", "\\\"")
+                    launch_script(
+                        f"python -m src.experiments.united.04_moeva "
+                        f"-c {config_dir}/moeva.yaml "
+                        f"-c {config_dir}/{project}.yaml "
+                        f"-p seed={seed} "
+                        f"-p budget={budget} "
+                        f"-j {eps_list_str} "
+                    )
 
                 # Run the rest
-                for eps in config["eps_list"]:
-                    logger.info(f"{TABULATOR * 4} Running eps {eps} ...")
+                if "pgd" in config["attacks"]:
+                    logger.info(f"{TABULATOR * 4} Running pgd ...")
+                    for eps in config["eps_list"]:
+                        logger.info(f"{TABULATOR * 5} Running eps {eps} ...")
 
-                    for loss_evaluation in config["loss_evaluations"]:
-                        logger.info(
-                            f"{TABULATOR * 5} Running loss_evaluation {loss_evaluation} ..."
-                        )
-                        launch_script(
-                            f"python -m src.experiments.united.01_pgd_united "
-                            f"-c {config_dir}/pgd.yaml "
-                            f"-c {config_dir}/{project}.yaml "
-                            f"-p seed={seed} "
-                            f"-p budget={budget} "
-                            f"-p eps={eps} "
-                            f"-p loss_evaluation={loss_evaluation} "
-                        )
+                        for loss_evaluation in config["loss_evaluations"]:
+                            logger.info(
+                                f"{TABULATOR * 6} Running loss_evaluation {loss_evaluation} ..."
+                            )
+                            launch_script(
+                                f"python -m src.experiments.united.01_pgd_united "
+                                f"-c {config_dir}/pgd.yaml "
+                                f"-c {config_dir}/{project}.yaml "
+                                f"-p seed={seed} "
+                                f"-p budget={budget} "
+                                f"-p eps={eps} "
+                                f"-p loss_evaluation={loss_evaluation} "
+                            )
 
 
 if __name__ == "__main__":
