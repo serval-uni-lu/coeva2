ad4215c35f804d672ae6672641d771b20f7c6d2a
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-08-05 16:19:10 +0200

    Compute metrics in moeva script directly

--- src/experiments/united/04_moeva.py
+++ src/experiments/united/04_moeva.py
@@ -1,16 +1,19 @@
 import warnings
-from datetime import datetime
+import time
 from pathlib import Path
 
 import joblib
 import numpy as np
 
+from src.attacks.moeva2.classifier import Classifier
 from src.attacks.moeva2.feature_encoder import get_encoder_from_constraints
 from src.attacks.moeva2.moeva2 import Moeva2
+from src.attacks.moeva2.objective_calculator import ObjectiveCalculator
 from src.attacks.moeva2.utils import results_to_numpy_results, results_to_history
 from src.config_parser.config_parser import get_config, get_config_hash, save_config
 from src.experiments.united.utils import get_constraints_from_str
-from src.utils import Pickler, filter_initial_states, timing
+from src.utils import Pickler, filter_initial_states, timing, in_out
+from src.utils.in_out import load_model
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.simplefilter(action="ignore", category=RuntimeWarning)
@@ -48,12 +51,8 @@ def run():
     # ----- Copy the initial states n_repetition times
     # X_initial_states = np.repeat(X_initial_states, config["n_repetition"], axis=0)
 
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
-
     n_gen = int(np.floor(config["budget"] / config["n_offsprings"]))
-
+    start_time = time.time()
     moeva = Moeva2(
         config["paths"]["model"],
         constraints,
@@ -71,7 +70,7 @@ def run():
         verbose=1,
     )
     attacks = moeva.generate(X_initial_states, 1)
-
+    consumed_time = time.time() - start_time
     # Save
     config_hash = get_config_hash()
     out_dir = config['dirs']['results']
@@ -87,7 +86,32 @@ def run():
     # History
     if save_history:
         x_histories = results_to_history(attacks)
-        np.save(f"{out_dir}/x_histories_{config_hash}.npy", x_histories)
+        np.save(f"{out_dir}/x_history_{config_hash}.npy", x_histories)
+
+
+    objective_lists = []
+    for eps in config["eps_list"]:
+        threholds = {"f1": config["misclassification_threshold"], "f2": eps}
+        classifier = Classifier(load_model(config["paths"]["model"]))
+        objective_calc = ObjectiveCalculator(
+            classifier,
+            constraints,
+            minimize_class=1,
+            thresholds=threholds,
+            min_max_scaler=scaler,
+            ml_scaler=scaler,
+        )
+        success_rate_df = objective_calc.success_rate_3d_df(X_initial_states, x_attacks)
+        objective_lists.append(success_rate_df.to_dict(orient="records")[0])
+
+    metrics = {
+        "objectives_list": objective_lists,
+        "time": consumed_time,
+        "config": config,
+        "config_hash": config_hash,
+    }
+    mid_fix = f"{config['attack_name']}"
+    in_out.json_to_file(metrics, f"{out_dir}/metrics_{mid_fix}_{config_hash}.json")
 
     # Config
     save_config(f"{out_dir}/config_")
