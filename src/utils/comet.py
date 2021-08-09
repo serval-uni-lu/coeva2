b7e7d8f7e9b52e678add881071650ccc956c1a17
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-08-09 15:56:50 +0200

    Comet config file

--- src/utils/comet.py
+++ src/utils/comet.py
@@ -2,19 +2,25 @@ from data.config import COMET_APIKEY
 from comet_ml import Experiment
 import time
 
+
 def init_comet(args, project_name="moeva", experiment_name=None):
     timestamp = time.time()
     args["timestamp"] = timestamp
     if experiment_name is None:
-        experiment_name = "{}_{}".format(args["approach"], timestamp)
+        experiment_name = "{}_{}".format(args["attack_name"], timestamp)
     else:
         experiment_name = "{}_{}".format(experiment_name, timestamp)
 
-    experiment = Experiment(api_key=COMET_APIKEY,
-                            project_name=project_name,
-                            workspace="yamizi",
-                            auto_param_logging=False, auto_metric_logging=False,
-                            parse_args=False, display_summary=False, disabled=False)
+    experiment = Experiment(
+        api_key=COMET_APIKEY,
+        project_name=project_name,
+        workspace="thibaultsmnt",
+        auto_param_logging=False,
+        auto_metric_logging=False,
+        parse_args=False,
+        display_summary_level=None,
+        disabled=False,
+    )
 
     experiment.set_name(experiment_name)
     experiment.log_parameters(args)
