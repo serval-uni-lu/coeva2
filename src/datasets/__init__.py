64397236b206fb1fe37f909cbfca8cefeaa4b61b
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-12-23 09:35:18 +0100

    Datasets load only on demand

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,13 +2,13 @@ from src.datasets.malware_dataset import MalwareDataset
 from src.datasets.url_dataset import UrlDataset
 
 datasets = {
-    "url": UrlDataset(),
-    "malware": MalwareDataset()
+    "url": UrlDataset,
+    "malware": MalwareDataset
 }
 
 
 def load_dataset(name: str):
     if name in datasets:
-        return datasets[name]
+        return datasets[name]()
     else:
         Exception("Object '%s' for not found in %s" % (name, datasets))
