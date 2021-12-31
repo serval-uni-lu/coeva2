a542bcc36f8a34005121354c6b9a657703403255
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-12-31 10:55:27 +0100

    Malware constraints

--- src/experiments/united/utils.py
+++ src/experiments/united/utils.py
@@ -1,27 +1,29 @@
-from src.datasets.malware_dataset import MalwareDataset
-from src.constraints.botnet.botnet_augmented_constraints import BotnetAugmentedConstraints
-from src.constraints.botnet.botnet_constraints import BotnetConstraints
-from src.constraints.lcld.lcld_augmented_constraints import LcldAugmentedConstraints
-from src.constraints.lcld.lcld_constraints import LcldConstraints
-from src.constraints.malware.malware_constraints import MalwareConstraintsFast
-from src.constraints.lcld.lcld_constraints_sat import (
-    create_constraints as lcld_sat_constraints,
+from src.constraints.botnet.botnet_augmented_constraints import (
+    BotnetAugmentedConstraints,
 )
+from src.constraints.botnet.botnet_constraints import BotnetConstraints
 from src.constraints.botnet.botnet_constraints_sat import (
     create_constraints as botnet_sat_constraints,
 )
+from src.constraints.lcld.lcld_augmented_constraints import LcldAugmentedConstraints
+from src.constraints.lcld.lcld_constraints import LcldConstraints
+from src.constraints.lcld.lcld_constraints_sat import (
+    create_constraints as lcld_sat_constraints,
+)
+from src.constraints.malware.malware_constraints import MalwareConstraints
 from src.constraints.malware.malware_constraints_sat import (
     create_constraints as malware_sat_constraints,
 )
 from src.constraints.url.url_constraints import UrlConstraints
+from src.datasets.malware_dataset import MalwareDataset
 
 STR_TO_CONSTRAINTS_CLASS = {
     "lcld": LcldConstraints,
     "botnet": BotnetConstraints,
-    "malware": MalwareConstraintsFast,
+    "malware": MalwareConstraints,
     "lcld_augmented": LcldAugmentedConstraints,
     "botnet_augmented": BotnetAugmentedConstraints,
-    "url": UrlConstraints
+    "url": UrlConstraints,
 }
 
 STR_TO_SAT_CONSTRAINTS = {
@@ -30,9 +32,7 @@ STR_TO_SAT_CONSTRAINTS = {
     "malware": malware_sat_constraints,
 }
 
-STR_TO_DATASET = {
-    "malware": MalwareDataset
-}
+STR_TO_DATASET = {"malware": MalwareDataset}
 
 
 def get_constraints_from_str(project_name: str):
@@ -41,6 +41,7 @@ def get_constraints_from_str(project_name: str):
 
 def get_sat_constraints_from_str(project_name: str):
     return STR_TO_SAT_CONSTRAINTS[project_name]
+
 
 def get_dataset(name: str):
     return STR_TO_DATASET[name]
