61d57f7237504b67f9778d81f26e0f764643eedb
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2022-01-07 14:04:19 +0100

    create project dirs helper script

---
+++ create_project_dirs.sh
@@ -0,0 +1,8 @@
+PROJECT_NAME=$1
+mkdir -p ./data/$PROJECT_NAME
+mkdir -p ./data/${PROJECT_NAME}_augmented
+mkdir -p ./models/${PROJECT_NAME}
+mkdir -p ./out/attacks/${PROJECT_NAME}/rq0
+mkdir -p ./out/attacks/${PROJECT_NAME}/rq1
+mkdir -p ./out/attacks/${PROJECT_NAME}/rq2
+mkdir -p ./out/attacks/${PROJECT_NAME}/rq3
